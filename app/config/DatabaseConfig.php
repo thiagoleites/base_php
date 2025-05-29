7c2d4e672b259fe6f37d311aa5931abfe8231160
Thiago Leite <thiagoleites@gmail.com>
2025-05-29 08:56:45 -0300

    refatora configuração do banco de dados para uma classe com método estático

--- app/config/DatabaseConfig.php
+++ app/config/DatabaseConfig.php
@@ -1,14 +1,20 @@
 <?php
 
-return [
-    'host' => 'localhost',
-    'database' => 'base_php',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4',
-    'options' => [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ]
-];
+namespace App\Config;
+use PDO;
+class DatabaseConfig {
+    public static function getConfig(): array {
+        return [
+            'host' => 'localhost',
+            'database' => 'seu_banco',
+            'username' => 'seu_usuario',
+            'password' => 'sua_senha',
+            'charset' => 'utf8mb4',
+            'options' => [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ]
+        ];
+    }
+}
